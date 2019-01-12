b480d6ee2b41ee15a2eff6f792c3bb9a4de176e2
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-01-12 15:05:28 -0500

    added npm init and install to cli

--- bin/commit.js
+++ bin/commit.js
@@ -9,34 +9,57 @@ const path = require("path");
 const fs = require("fs");
 const makeDir = require("make-dir");
 const cpx = require("cpx");
+const slash = require("slash");
+const ora = require("ora");
+const execSync = require("child_process").execSync;
+
+const exec = (command, extraEnv) =>
+  execSync(command, {
+    stdio: "inherit",
+    env: Object.assign({}, process.env, extraEnv)
+  });
 
 console.log(
   chalk.green(
     figlet.textSync("React Template", {
-      font: "cybermedium",
-      horizontalLayout: "default",
-      verticalLayout: "default"
+      font: "cybermedium"
     })
-  ),
-  chalk.yellow(`\nv${version}`)
+  )
 );
+console.log(chalk.black.bgWhite(`v${version}`));
 
 commander
   .version(version)
-  .option("-d, --dir [value]", "directory")
-  .arguments("<project-directory> [options]")
+  .arguments("<project-directory>")
   .action(async dir => {
     console.log("Creating project directory:", dir);
 
-    const path = await makeDir(dir);
-    console.log("Using path:", path);
-    cpx.copySync("template/!(node_modules)", path, { includeEmptyDirs: true });
-    cpx.copySync("template/.*", path);
+    const pathDirectory = await makeDir(dir);
+    console.log("Using path:", pathDirectory);
+
+    const relativePath = slash(path.relative(process.cwd(), __dirname));
+
+    console.log("relative", `${relativePath}/template/!(node_modules)`);
+
+    const spinner = ora("Copying files").start();
+    cpx.copySync(`${relativePath}/../template/!(node_modules)`, pathDirectory, {
+      includeEmptyDirs: true,
+      verbose: true
+    });
+    spinner.color = "green";
+    cpx.copySync(`${relativePath}/../template/.*`, pathDirectory);
+    spinner.stop();
+
     console.log("Done copying files");
+
+    console.log(`cd ${dir}`);
+    shell.cd(dir);
+    console.log(process.cwd());
+    exec("npm init");
+    console.log(chalk.green("Installing dependencies..."));
+    exec("npm i");
+    console.log('Run "npm run start" to start the project');
+    console.log('Run "npm run build" to build the project');
   });
 
 commander.parse(process.argv);
-
-shell.exec("echo shell.exec works");
-
-// console.log(process.cwd(), "\n", __dirname);
